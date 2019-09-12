a7382d012336a3adfd9d6f064f3dd62d05d4d306
brandonlind <lindb@vcu.edu>
2019-09-12 15:04:23 -0700

    research

---
+++ test.py
@@ -0,0 +1,7 @@
+app.layout = html.Div([ # page 1
+    html.Div([
+        html.Div(
+            html.Img(src='https://raw.githubusercontent.com/brandonlind/brandonlind.github.io/master/images/TEST_bannertype_3.png',height="80%")
+            ,style={"position":"center","height":"20vh"})
+        ],className="row header"),
+])
